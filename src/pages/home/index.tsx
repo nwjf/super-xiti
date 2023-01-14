bc9c368ac8f071a094436463aded2834d78093b1
newwjf <newwjf@outlook.com>
2023-01-14 22:40:39 +0800

    wip:删除不必要组件

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,29 +3,16 @@
  */
 
 import './index.less';
-import PageView from './components/pageView';
-import PageConfig from './components/pageConfig';
-import OperationView from './components/modules/operation/view';
-import { useGetContentDataState } from '../../atoms/contentData';
 import ConfigPanel from './components/configPanel';
 import ContentPanel from './components/contentPanel';
 
 export default function Home() {
-  const { moduleList } = useGetContentDataState();
-  console.log('modulelist', moduleList);
   return (
     <div className="home-warp">
       <ConfigPanel />
       <div className="home-content">
         <ContentPanel />
       </div>
-      <PageConfig style={{ marginBottom: '10px' }} />
-      <PageView>
-        {/* <OperationView /> */}
-        {
-          moduleList.map((item: any) => <OperationView moduleData={item} key={item.id}/>)
-        }
-      </PageView>
     </div>
   );
 };

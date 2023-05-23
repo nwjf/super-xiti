e9c6e5bdb6e9ac2a55f509e70b93a78ef97ce2aa
newwjf <newwjf@outlook.com>
2023-05-23 15:53:12 +0800

    fix:修改operation中创建规则中去重排序进位问题

--- src/pages/operation/components/createConfig/index.tsx
+++ src/pages/operation/components/createConfig/index.tsx
@@ -2,7 +2,7 @@
  * 生成数据配置
  */
 
-import { Select, Row, Col, Input, Button } from 'antd';
+import { Select, Row, Col, Input, Button, message } from 'antd';
 import MenuItem from '../../../../components/menuItem';
 import { useState, useCallback } from 'react';
 
@@ -26,11 +26,20 @@ export default function CreateConfig() {
     bMax: 100,
     opertionType: '+',
     createTotal: 10,
+    carry: 'r', // 进位/退位 , 随机 y/n/r
   });
 
   const optionsList = [
     { label: 0, value: 0 },
     { label: 1, value: 1 },
+    { label: 2, value: 2 },
+    { label: 3, value: 3 },
+    { label: 4, value: 4 },
+    { label: 5, value: 5 },
+    { label: 6, value: 6 },
+    { label: 7, value: 7 },
+    { label: 8, value: 8 },
+    { label: 9, value: 9 },
     { label: 10, value: 10 },
     { label: 20, value: 20 },
     { label: 30, value: 30 },
@@ -43,6 +52,12 @@ export default function CreateConfig() {
     { label: 100, value: 100 },
     { label: 150, value: 150 },
     { label: 200, value: 200 },
+  ];
+
+  const carryList = [
+    { label: '进位/退位', value: 'y' },
+    { label: '不进位/不退位', value: 'n' },
+    { label: '随机', value: 'r' },
   ];
 
   const onValueChange = useCallback((data: any, key: string) => {
@@ -62,16 +77,63 @@ export default function CreateConfig() {
       bMax,
       opertionType,
       createTotal,
+      carry,
     } = createDataConfig;
-    let list = [];
-    for (let i = 0; i < createTotal; i++) {
+    let list: Array<any> = [];
+
+    let forNum = 0;
+    for (;;) {
+      // 防止内存超出
+      forNum++;
+      if (forNum / createTotal > 5) {
+        message.error('条件可能超出，生成数据失败，请调整后再次尝试');
+        return;
+      }
+
+
       const a = random(aMin, aMax);
       const b = random(bMin, bMax);
       let c = 0;
+
+      // 去重
+      let isContinue = false;
+      for (let i = 0; i < list.length; i++) {
+        let isTrue = list[i].opertionType === opertionType
+          && (
+            (list[i].a === a && list[i].b === b)
+            || (list[i].a === b && list[i].b === a)
+          );
+        if (isTrue) {
+          isContinue = true;
+          break;
+        }
+      }
+      if (isContinue) {
+        continue;
+      }
+
       if (opertionType === '+') { c = a + b; }
       if (opertionType === '-') { c = a - b; }
       if (opertionType === '*') { c = a * b; }
       if (opertionType === '/') { c = a / b; }
+
+      // 进位/退位
+      if (carry === 'y' && (opertionType === '+' || opertionType === '-')) {
+        const aStr = a.toString();
+        const cStr = c.toString();
+        if (aStr.length === cStr.length && aStr[0] === cStr[0]) {
+          continue;
+        }
+      }
+      // 不进/不退
+      if (carry === 'n' && (opertionType === '+' || opertionType === '-')) {
+        const aStr = a.toString();
+        const cStr = c.toString();
+        if (aStr.length !== cStr.length || aStr[0] !== cStr[0]) {
+          continue;
+        }
+      }
+
       const temp = {
         a,
         b,
@@ -79,9 +141,17 @@ export default function CreateConfig() {
         str: `${a} ${opertionType} ${b} = ${c}`,
         opertionType: opertionType,
       };
+
       list.push(temp);
+
+      if (list.length >= createTotal) {
+        break;
+      }
     }
-    list = list.sort((a, b) => a.a - b.a)
+    
+    // 排序
+    list = list.sort((a, b) => a.a - b.a);
+
     setOperationData([...operationConfigState.data, ...list]);
   }, [createDataConfig, operationConfigState.data]);
 
@@ -151,6 +221,20 @@ export default function CreateConfig() {
               options={optionsList}
               onChange={(d) => onValueChange(d, 'createTotal')} />
           </MenuItem>
+        </Col>
+      </Row>
+
+      <Row gutter={20}>
+        <Col span={12}>
+          <MenuItem name="进位(+)/退位(-)">
+            <Select
+              style={{width: '100%'}}
+              value={createDataConfig.carry}
+              options={carryList}
+              onChange={(d) => onValueChange(d, 'carry')} />
+          </MenuItem>
+        </Col>
+        <Col span={12}>
         </Col>
       </Row>
 

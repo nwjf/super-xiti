500a7e68ec8ec645905ae8a98e06749c4dc2a26c
newwjf <newwjf@outlook.com>
2026-01-04 17:40:31 +0800

    fix: 修改导航

--- src/components/navbar/path.ts
+++ src/components/navbar/path.ts
@@ -1,12 +1,17 @@
-
+import type { GetProp, MenuProps } from 'antd';
 export interface Path {
   label: string;
   key: string;
   path: string;
+  type?: string;
   target?: '_balnk' | '';
   children?: Path[]
 };
-export const pathList: Path[] = [
+type MenuItem = GetProp<MenuProps, 'items'>[number];
+
+type PathList = MenuItem & Path;
+
+export const pathList: PathList[] = [
   { label: '官网', key: 'index', path: '/index', target: '' },
   { label: '运算题', key: 'operation', path: '/operation', target: '' },
   { label: '中文', key: 'chinese', path: '/chinese', target: '' },
